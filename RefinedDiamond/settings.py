665f0e41afe9cea4fe0623e9141e240f52db9611
PRINCEOYEBOLA <65773116+PRINCEOYEBOLA@users.noreply.github.com>
2022-06-25 16:24:20 +0100

    Add files via upload

--- RefinedDiamond/settings.py
+++ RefinedDiamond/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0+y9nbi8a0krqbnhdqfc*10ri+x++@k!ffmv!(d5*1-q0l5iq%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homepage'
+    'homepage.apps.HomepageConfig',
 ]
 
 MIDDLEWARE = [
